b3576093ed19c8e1596006d9883a59b5dfebc093
chumpon-asaneerat <chumponsenate@gmail.com>
2020-01-02 19:18:35 +0700

    Update rawvote report #10

--- routes/api/customer/report/votesummaries/routes.js
+++ routes/api/customer/report/votesummaries/routes.js
@@ -7,14 +7,13 @@ const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
 const sqldb = require(path.join(nlib.paths.root, 'RaterWebv2x08r9.db'));
 const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
 
+const rptAPI = require('../report-api').api;
+
 const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
 const WebRouter = WebServer.WebRouter;
 const router = new WebRouter();
 
 //#endregion
-
-const fs = require('fs')
-const mkdirp = require('mkdirp')
 
 //#region exec/validate wrapper method
 
@@ -53,99 +52,6 @@ const checkForError = (data) => {
 //#endregion
 
 const api = class {
-    static async GetQSet(db, params) {
-        let ret = {};
-        let qslideitemsDbRet = await db.GetQSlideItems({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            qSeq: null,
-            qSSeq: null,
-            enabled: true,
-        });
-        let qslideitems = qslideitemsDbRet.data;
-        for (let i = 0; i < qslideitems.length; i++) {
-            let row = qslideitems[i];
-            if (!ret[row.langId]) {
-                ret[row.langId] = {
-                    desc: '',
-                    beginDate: null,
-                    endDate: null,
-                    slides: []
-                }
-            }
-            let currLang = ret[row.langId];
-            let slidemaps = currLang.slides.map(slide => { return slide.qseq })
-            let slideidx = slidemaps.indexOf(row.qSeq)
-            let currSlide;
-            if (slideidx === -1) {
-                currSlide = {
-                    qseq: row.qSeq,
-                    text: '',
-                    items: []
-                }
-                currLang.slides.push(currSlide)
-            }
-            else {
-                currSlide = currLang.slides[slideidx]
-            }
-            let itemmaps = currSlide.items.map(item => { return item.choice })
-            let itemidx = itemmaps.indexOf(row.qSSeq)
-            let currItem;
-            if (itemidx === -1) {
-                currItem = {
-                    choice: row.qSSeq,
-                    text: row.QItemText
-                }
-                currSlide.items.push(currItem)
-            }
-            else {
-                currItem = currSlide.items[itemidx]
-            }
-        }
-
-        let qslidesDbRet = await db.GetQSlides({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            qSeq: null,
-            enabled: true,
-        });
-        let qslides = qslidesDbRet.data;
-        for (let i = 0; i < qslides.length; i++) {
-            let row = qslides[i];
-            let currLang = ret[row.langId];
-            if (currLang) {
-                let slidemaps = currLang.slides.map(slide => { return slide.qseq })
-                let slideidx = slidemaps.indexOf(row.qSeq)
-                let currSlide;
-                if (slideidx !== -1) {
-                    currSlide = currLang.slides[slideidx]
-                    currSlide.text = row.QSlideText;
-                }
-            }
-        }
-
-        let qsetDbRet = await db.GetQSets({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            enabled: true,
-        });
-        let qsets = qsetDbRet.data;
-
-        for (let i = 0; i < qsets.length; i++) {
-            let row = qsets[i];
-            let currLang = ret[row.langId];
-            if (currLang) {
-                currLang.desc = row.QSetDescription;
-                currLang.beginDate = row.BeginDate;
-                currLang.endDate = row.EndDate;
-            }
-        }        
-
-        return ret;
-    }
     static CreateVoteSummaries(obj, qset, results) {
         if (results && results.length > 0) {
             for (let i = 0; i < results.length; i++) {
@@ -235,224 +141,6 @@ const api = class {
             }
         }
     }
-
-    static findIndex(items, property, value) {
-        let maps = items.map(item => { return item[property] })
-        let idx = maps.indexOf(value)
-        return idx;
-    }
-}
-
-// static class.
-/*
-const api = class {
-    static findIndex(items, property, value) {
-        let maps = items.map(item => { return item[property] })
-        let idx = maps.indexOf(value)
-        return idx;
-    }
-}
-*/
-api.question = class {
-    static checkLanguageId(params) {
-        if (params.langId === undefined || params.langId === null || params.langId === '') {
-            params.langId = null;
-        }
-    }
-    static prepare(params) {
-        let util = api.question;
-        util.checkLanguageId(params)
-    }
-    static async load(db, params) {
-        let util = api.question;
-        util.prepare(params)
-        let ret = {};
-        await util.qslideitems.exec(db, params, ret)
-        await util.qslides.exec(db, params, ret)
-        await util.qsets.exec(db, params, ret)
-        return ret;
-    }
-    static parse(db, params, data) {
-        let oParams = {}
-        oParams.langId = params.langId;
-        oParams.customerId = params.customerId;
-        oParams.beginDate = params.beginDate;
-        oParams.endDate = params.endDate;
-        oParams.qsetId = params.qsetId;
-
-        let slides = params.slides;
-        let orgs = params.orgs;
-
-        let result;
-
-        if (slides && slides.length > 0) {
-
-        }
-        else {
-
-        }
-
-        result = {
-            data: null,
-            errors: { hasError: false, errNum: 0, errMsg: '' },
-            out: {}
-        }
-        // set to result.
-        result.data = data;
-
-        return result;
-    }
-    static parseSlides(db, params, data, slides, orgs) {
-
-    }
-}
-api.question.qslideitems = class {
-    static async exec(db, params, model) {
-        let util = api.question.qslideitems; // shotcut
-        let rows = await util.exec_db(db, params)
-        util.process_rows(model, rows)
-    }
-    static async exec_db(db, params) {
-        let dbResult = await db.GetQSlideItems({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            qSeq: null,
-            qSSeq: null,
-            enabled: true,
-        });
-        return (dbResult) ? dbResult.data : null;
-    }
-    static process_rows(model, rows) {
-        let util = api.question.qslideitems; // shotcut
-        for (let i = 0; i < rows.length; i++) {
-            let row = rows[i];
-            util.process_row(model, row)
-        }
-    }
-    static process_row(model, row) {
-        let util = api.question.qslideitems; // shotcut
-        util.checkLanguagePropperty(model, row)
-        let langObj = model[row.langId];
-        let slideObj = util.getSlideByRow(row, langObj)
-        let slideItemObj = util.getSlideItemByRow(row, slideObj)
-    }
-    static checkLanguagePropperty(model, row) {
-        if (!model[row.langId]) {
-            model[row.langId] = {
-                desc: '',
-                beginDate: null,
-                endDate: null,
-                slides: []
-            }
-        }
-    }
-    static getSlideByRow(row, langObj) {
-        let util = api.question.qslideitems; // shotcut
-        let ret;
-        let idx = api.findIndex(langObj.slides, 'qseq', row.qSeq)
-        if (idx === -1) {
-            ret = {
-                qseq: row.qSeq,
-                text: '',
-                items: []
-            }
-            langObj.slides.push(ret)
-        }
-        else {
-            ret = langObj.slides[idx]
-        }
-        return ret;
-    }
-    static getSlideItemByRow(row, slideObj) {
-        let util = api.question.qslideitems; // shotcut
-        let ret;
-        let idx = api.findIndex(slideObj.items, 'choice', row.qSSeq)
-        if (idx === -1) {
-            ret = {
-                choice: row.qSSeq,
-                text: row.QItemText
-            }
-            slideObj.items.push(ret)
-        }
-        else {
-            ret = currSlide.items[idx]
-        }
-        return ret;
-    }
-}
-api.question.qslides = class {
-    static async exec(db, params, model) {
-        let util = api.question.qslides; // shotcut
-        let rows = await util.exec_db(db, params)
-        util.process_rows(model, rows)
-    }
-    static async exec_db(db, params) {
-        let dbResult = await db.GetQSlides({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            qSeq: null,
-            enabled: true,
-        });
-        return (dbResult) ? dbResult.data : null;
-    }
-    static process_rows(model, rows) {
-        let util = api.question.qslides; // shotcut
-        for (let i = 0; i < rows.length; i++) {
-            let row = rows[i];
-            util.process_row(model, row)
-        }
-    }
-    static process_row(model, row) {
-        let util = api.question.qslides; // shotcut
-        let langObj = model[row.langId];
-        if (langObj) {
-            let slideObj = util.getSlideByRow(row, langObj)
-        }        
-    }
-    static getSlideByRow(row, langObj) {
-        //let util = api.question.qslides; // shotcut
-        let idx = api.findIndex(langObj.slides, 'qseq', row.qSeq)
-        let ret = null;
-        if (idx !== -1) {
-            ret = langObj.slides[idx];
-            ret.text = row.QSlideText;
-        }
-        return ret; 
-    }
-}
-api.question.qsets = class {
-    static async exec(db, params, model) {
-        let util = api.question.qsets; // shotcut
-        let rows = await util.exec_db(db, params)
-        util.process_rows(model, rows)
-    }
-    static async exec_db(db, params) {
-        let dbResult = await db.GetQSets({
-            langId: null,
-            customerId: params.customerId,
-            qsetId: params.qsetId,
-            enabled: true,
-        });
-        return (dbResult) ? dbResult.data : null;
-    }
-    static process_rows(model, rows) {
-        let util = api.question.qsets; // shotcut
-        for (let i = 0; i < rows.length; i++) {
-            let row = rows[i];
-            util.process_row(model, row)
-        }
-    }
-    static process_row(model, row) {
-        let util = api.question.qsets; // shotcut
-        let langObj = model[row.langId];
-        if (langObj) {
-            langObj.desc = row.QSetDescription;
-            langObj.beginDate = row.BeginDate;
-            langObj.endDate = row.EndDate;
-        }
-    }
 }
 
 //#region Implement - Get
@@ -487,7 +175,7 @@ api.Get = class {
         oParams.endDate = params.endDate;
         oParams.qsetId = params.qsetId;
 
-        let qset = await api.GetQSet(db, params);
+        let qset = await rptAPI.question.load(db, params);
 
         console.log(qset)
 
@@ -680,7 +368,7 @@ router.all('/report/votesummaries/test1', (req, res) => {
     let db = new sqldb();
     let params = api.Get.prepare(req, res);
     let fn = async () => { 
-        return await api.question.load(db, params);
+        return await rptAPI.question.load(db, params);
     }
     exec(db, fn).then(data => {
         WebServer.sendJson(req, res, data);
