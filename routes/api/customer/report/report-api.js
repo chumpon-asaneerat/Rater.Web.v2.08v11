1d8af680e5fec12aa40bf121de3059cbef4acfde
chumpon-asaneerat <chumponsenate@gmail.com>
2020-01-04 20:43:55 +0700

    Update rawvote report #31

--- routes/api/customer/report/report-api.js
+++ routes/api/customer/report/report-api.js
@@ -416,4 +416,198 @@ api.votesummary = class {
     }
 }
 
+api.staffsummary = class {
+    static async processSlides(db, params, slides, orgs, result, qset) {
+        let oParams = {};
+        oParams.langId = params.langId;
+        oParams.customerId = params.customerId;
+        oParams.beginDate = params.beginDate;
+        oParams.endDate = params.endDate;
+        oParams.qsetId = params.qsetId;
+
+        for (let i = 0; i < slides.length; i++) {
+            oParams.qSeq = slides[i].qSeq;
+            if (api.isEmpty(orgs)) {
+                await api.staffsummary.processOrgs(db, oParams, orgs, result, qset)
+            }
+            else {
+                // no org specificed
+                await api.staffsummary.processNoOrg(db, oParams, result, qset)
+            }
+        }
+    }
+    static async processNoSlide(db, params, orgs, result, qset) {
+        let oParams = {};
+        oParams.langId = params.langId;
+        oParams.customerId = params.customerId;
+        oParams.beginDate = params.beginDate;
+        oParams.endDate = params.endDate;
+        oParams.qsetId = params.qsetId;
+
+        // no slide specificed
+        oParams.qSeq = null;
+        if (api.isEmpty(orgs)) {
+            await api.staffsummary.processOrgs(db, oParams, orgs, result, qset)
+        }
+        else {
+            // no org specificed
+            await api.staffsummary.processNoOrg(db, oParams, result, qset)
+        }
+    }
+    static async processOrgs(db, params, orgs, result, qset) {
+        let dbresult;
+        for (let j = 0; j < orgs.length; j++) {
+            params.orgId = orgs[j].orgId;
+            // execute
+            dbresult = await api.staffsummary.GetVoteSummaries(db, params);
+            api.staffsummary.CreateStaffSummaries(result, qset, dbresult.data)
+        }
+    }
+    static async processNoOrg(db, params, result, qset) {
+        // no org specificed
+        let dbresult;
+        params.orgId = null;
+        // execute
+        dbresult = await api.staffsummary.GetVoteSummaries(db, params);
+        api.staffsummary.CreateStaffSummaries(result, qset, dbresult.data)
+    }
+    static async GetVoteSummaries(db, params) {
+        let ret, dbresult;
+        ret = await db.GetVoteSummaries(params);
+        dbresult = validate(db, ret);
+        return dbresult;
+    }
+    static CreateStaffSummaries(obj, qset, results) {
+        if (results && results.length > 0) {
+            for (let i = 0; i < results.length; i++) {
+                let row = results[i];
+                api.staffsummary.ParseVoteSummaryRow(obj, qset, row)
+            }
+        }
+    }
+    static ParseVoteSummaryRow(obj, qset, row) {
+        let cQSet = qset[row.LangId]
+        let cqslideidx = api.findIndex(cQSet.slides, 'qseq', row.QSeq) 
+        let cQSlide = (cqslideidx !== -1) ? cQSet.slides[cqslideidx] : null;
+
+        let cLangObj = api.votesummary.GetLangObj(obj, row, cQSlide)
+        let currSlide = api.votesummary.GetCurrentSlide(row, cLangObj, cQSlide)
+        let currOrg = api.votesummary.GetCurrentOrg(row, currSlide)
+        api.staffsummary.GetOrgChoice(row, cQSlide, currOrg)
+    }
+    static GetLangObj(obj, row, cQSet) {
+        let landId = row.LangId;
+        if (!obj[landId]) {
+            obj[landId] = {
+                slides: []
+            }
+        }
+        let ret = obj[landId]
+        ret.customerId = row.CustomerId;
+        ret.CustomerName = row.CustomerName;
+        ret.qsetId = row.QSetId;
+        ret.desc = cQSet.desc;
+        ret.beginDate = cQSet.beginDate;
+        ret.endDate = cQSet.endDate;
+        return ret;
+    }
+    static GetCurrentSlide(row, cLangObj, cQSlide) {
+        let ret;
+        let slideidx = api.findIndex(cLangObj.slides, 'qseq', row.QSeq)
+        if (slideidx === -1) {
+            ret = { 
+                qseq: row.QSeq,
+                text: (cQSlide) ? cQSlide.text : '',
+                maxChoice: row.MaxChoice,
+                choices: [],
+                orgs: []
+            }
+            // setup choices
+            api.staffsummary.setupSlideChoices(ret, cQSlide)
+            cLangObj.slides.push(ret)
+        }
+        else {
+            ret = cLangObj.slides[slideidx];
+        }
+        return ret;
+    }
+    static setupSlideChoices(result, cQSlide) {
+        if (cQSlide && cQSlide.items.length > 0) {
+            for (let i = 0; i < cQSlide.items.length; i++) {
+                let item = cQSlide.items[i]
+                let choice = {
+                    choice: item.choice,
+                    text: item.text,
+                }
+                result.choices.push(choice)
+            }
+        }
+    }
+    static GetCurrentOrg(row, currSlide) {
+        let orgidx = api.findIndex(currSlide.orgs, 'orgId', row.OrgId)
+        let ret;
+        if (orgidx === -1) {
+            ret = { 
+                orgId: row.OrgId,
+                OrgName: row.OrgName,
+                parentId: row.ParentId,
+                branchId: row.BranchId,
+                BranchName: row.BranchName,
+                TotCnt: row.TotCnt,
+                AvgPct: row.AvgPct,
+                AvgTot: row.AvgTot,
+                choices: []
+            }
+            currSlide.orgs.push(ret)
+        }
+        else { 
+            ret = currSlide.orgs[orgidx];
+        }
+        return ret;
+    }
+    static GetOrgChoice(row, cQSlide, currOrg) {
+        let choiceidx = api.findIndex(currOrg.choices, 'choice', row.Choice)
+        let ret;
+        if (choiceidx === -1) {
+            ret = {
+                choice: row.Choice,
+                text: api.staffsummary.getOrgChoiceText(row, cQSlide),
+                Cnt: row.Cnt,
+                Pct: row.Pct,
+            }
+            currOrg.choices.push(ret)
+        }
+        else {
+            ret = currOrg.choices[choiceidx];
+        }
+        return ret;
+    }
+    static getOrgChoiceText(row, cQSlide) {
+        let ret = ''
+        if (cQSlide && cQSlide.items.length > 0) {
+            let cQItemidx = api.findIndex(cQSlide.items, 'choice', row.Choice)
+            if (cQItemidx !== -1) {
+                ret = cQSlide.items[cQItemidx].text;
+            }
+        }
+        return ret
+    }
+    static async load(db, params) {
+        let qset = await api.question.load(db, params);
+
+        let slides = params.slides;
+        let orgs = params.orgs;
+        let result = {};
+        
+        if (api.isEmpty(slides)) {
+            await api.staffsummary.processSlides(db, params, slides, orgs, result, qset)
+        }
+        else {
+            await api.staffsummary.processNoSlide(db, params, orgs, result, qset)
+        }
+
+        return result;
+    }
+}
+
 module.exports.api = exports.api = api;
