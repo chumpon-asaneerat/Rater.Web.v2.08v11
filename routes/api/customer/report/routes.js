ff8c9dd7138631dbb823f254b04b1994cc025a96
chumpon-asaneerat <chumponsenate@gmail.com>
2020-01-06 01:44:30 +0700

    Update rawvote report #37

---
+++ routes/api/customer/report/routes.js
@@ -0,0 +1,70 @@
+//#region requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+
+const sqldb = require(path.join(nlib.paths.root, 'RaterWebv2x08r9.db'));
+const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+//#region exec/validate wrapper method
+
+const exec = async (db, callback) => {
+    let ret;
+    let connected = await db.connect();
+    if (connected) {
+        ret = await callback();
+        await db.disconnect();
+    }
+    else {
+        ret = db.error(db.errorNumbers.CONNECT_ERROR, 'No database connection.');
+    }
+    return ret;
+}
+const validate = (db, data) => {
+    let result = data;
+    if (!result) {
+        result = db.error(db.errorNumbers.NO_DATA_ERROR, 'No data returns');
+    }
+    else {
+        result = checkForError(data);
+    }
+    return result;
+}
+const checkForError = (data) => {
+    let result = data;
+    if (result.out && result.out.errNum && result.out.errNum !== 0) {
+        result.errors.hasError = true;
+        result.errors.errNum = result.out.errNum;
+        result.errors.errMsg = result.out.errMsg;
+    }
+    return result;
+}
+
+//#endregion
+
+const api = class {}
+api.filter = class {}
+api.filter.member = class {
+    static get(req, res) {}
+}
+api.filter.qset = class {
+    static get(req, res) {}
+}
+
+router.use(secure.checkAccess);
+// routes for staff summaries
+router.all('/report/filter/member', api.filter.member.get);
+router.all('/report/filter/qset', api.filter.qset.get);
+
+const init_routes = (svr) => {
+    svr.route('/customer/api/', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
