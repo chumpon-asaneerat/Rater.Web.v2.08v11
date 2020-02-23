76c8a736a474e846811b78afe7923838721cf640
chumpon-asaneerat <chumponsenate@gmail.com>
2020-02-24 00:42:44 +0700

    Update fabric.js sample #13.

--- routes/views/dev/fabric.js/07 - Selection/nlib-fabric.js
+++ routes/views/dev/fabric.js/07 - Selection/nlib-fabric.js
@@ -1,3 +1,26 @@
+//#region Helper methods
+
+let toRGBA = (hexString) => {
+    let h = hexString;
+    let r = 0, g = 0, b = 0, a = 1;
+    if (h.length === 5) {
+      r = "0x" + h[1] + h[1];
+      g = "0x" + h[2] + h[2];
+      b = "0x" + h[3] + h[3];
+      a = "0x" + h[4] + h[4];
+    }
+    else if (h.length === 9) {
+      r = "0x" + h[1] + h[2];
+      g = "0x" + h[3] + h[4];
+      b = "0x" + h[5] + h[6];
+      a = "0x" + h[7] + h[8];
+    }
+    a = +(a / 255).toFixed(3);
+    return "rgba(" + r + ", " + g + ", " + b + ", " + a + ")";
+}
+
+//#endregion
+
 //#region Helper classes.
 
 /**
@@ -63,7 +86,6 @@ class GIFFrame {
 // Button
 
 // MultilanguageText
-
 
 //#endregion
 
