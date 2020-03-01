496ce1fb0cfd79d044bdae10a0ea3aa1d38422b3
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-01 13:05:23 +0700

    Add center div sample #1

---
+++ routes/views/dev/css/08 - Center div/routes.js
@@ -0,0 +1,24 @@
+//#region common requires
+
+const path = require('path');
+const rootPath = process.env['ROOT_PATHS'];
+const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
+const WebRouter = WebServer.WebRouter;
+const router = new WebRouter();
+
+//#endregion
+
+const routes = class {
+    static home(req, res) {
+        WebServer.sendFile(req, res, __dirname, 'index.html');
+    }
+}
+
+router.get('/css/center-div1', routes.home)
+
+const init_routes = (svr) => {
+    svr.route('/dev', router);
+};
+
+module.exports.init_routes = exports.init_routes = init_routes;
