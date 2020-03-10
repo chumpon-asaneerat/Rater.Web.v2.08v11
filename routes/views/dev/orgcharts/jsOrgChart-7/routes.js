b613e9fb8fb6e7221569de6ffb7948fb1e997515
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-10 08:57:03 +0700

    2020-03-10 #4 - Update fixed riot sample - device app tag. - Update route for all css samples. - Update route for all fabricjs samples. - Update route for all layouts samples. - Update route for all jsorgchart samples.

--- routes/views/dev/orgcharts/jsOrgChart-7/routes.js
+++ routes/views/dev/orgcharts/jsOrgChart-7/routes.js
@@ -1,8 +1,11 @@
-//#region common requires
+//#region requires
 
 const path = require('path');
 const rootPath = process.env['ROOT_PATHS'];
 const nlib = require(path.join(rootPath, 'nlib', 'nlib'));
+const sfs = require(path.join(rootPath, 'edl', 'server-fs'));
+//const secure = require(path.join(rootPath, 'edl', 'rater-secure')).RaterSecure;
+
 const WebServer = require(path.join(rootPath, 'nlib', 'nlib-express'));
 const WebRouter = WebServer.WebRouter;
 const router = new WebRouter();
