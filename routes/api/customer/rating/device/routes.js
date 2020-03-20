70346e2d550526a6d7b7edd04b3279a442c0c714
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-20 10:15:11 +0700

    Update 2020-03-20 #1 - Add rating/device route search implements (for check is deviceId is set) - Add rating/device route save implements (for register/unregister)

--- routes/api/customer/rating/device/routes.js
+++ routes/api/customer/rating/device/routes.js
@@ -58,71 +58,23 @@ const api = class { }
 //#region Implement - Get
 
 api.Get = class {
-    static prepare(req, res) {
-        let params = WebServer.parseReq(req).data;
-        // force langId to null;
-        params.langId = null;
+    static entry(req, res) {
+        /*
+        let accessId = secure.getAccessId(req, res);
+        if (accessId) params.accessId = accessId;
         let customerId = secure.getCustomerId(req, res);
         if (customerId) params.customerId = customerId;
-        params.deviceId = null;
-        params.enabled = true;
-
-        return params;
-    }
-    static async call(db, params) { 
-        return db.GetDevices(params);
-    }
-    static parse(db, data, callback) {
-        let dbResult = validate(db, data);
-
+        let deviceId = secure.getDeviceId(req, res);
+        if (deviceId) params.deviceId = deviceId;
+        */
         let result = {
-            data : null,
-            //src: dbResult.data,
-            errors: dbResult.errors,
-            //multiple: dbResult.multiple,
-            //datasets: dbResult.datasets,
-            out: dbResult.out
+            data : {},
+            errors: null,
+            out: {}
         }
-        let records = dbResult.data;
-        let ret = {};
-
-        records.forEach(rec => {
-            if (!ret[rec.langId]) {
-                ret[rec.langId] = []
-            }
-            let map = ret[rec.langId].map(c => c.deviceId);
-            let idx = map.indexOf(rec.deviceId);
-            let nobj;
-            if (idx === -1) {
-                // set id
-                nobj = { deviceId: rec.deviceId }
-                // init lang properties list.
-                ret[rec.langId].push(nobj)
-            }
-            else {
-                nobj = ret[rec.langId][idx];
-            }
-            nobj.DeviceName = rec.DeviceName;
-            nobj.Location = rec.Location;
-            nobj.deviceTypeId = rec.deviceTypeId;
-            nobj.memberId = rec.memberId;
-            nobj.orgId = rec.orgId;
-            nobj.Type = rec.Type;
-        })
-        // set to result.
-        result.data = ret;
-
-        callback(result);
-    }
-    static entry(req, res) {
-        let db = new sqldb();
-        let params = api.Get.prepare(req, res);
-        let fn = async () => { return api.Get.call(db, params); }
-        exec(db, fn).then(data => {
-            api.Get.parse(db, data, (result) => {
-                WebServer.sendJson(req, res, result);
-            });
-        })
+        result.data.customerId = secure.getCustomerId(req, res)
+        result.data.deviceId = secure.getDeviceId(req, res)
+        WebServer.sendJson(req, res, result);
     }
 }
 
@@ -134,65 +86,23 @@ api.Save = class {
     static prepare(req, res) {
         let params = WebServer.parseReq(req).data;
 
+        let accessId = secure.getAccessId(req, res);
+        if (accessId) params.accessId = accessId;
         let customerId = secure.getCustomerId(req, res);
         if (customerId) params.customerId = customerId;
+        // required value or null but required
+        //params.deviceId = null; // reset
 
         return params;
     }
     static async call(db, params) { 
-        let ret;
-        let rets = [];
-        let customerId = params.customerId;
-        if (params && params.items) {
-            let items = params.items;
-            let deviceId;
-            // loop to save EN item as default and 
-            // keep device id when create new.
-            for (let i = 0; i < items.length; i++) {
-                let item = items[i];
-                item.customerId = customerId;
-                if (item.langId === 'EN') {
-                    ret = await db.SaveDevice(item);
-                    deviceId = ret.out.deviceId;
-                    rets.push(ret);
-                }
-            }
-            // loop to save non EN items and 
-            // assign parent device id when save in child table.
-            for (let i = 0; i < items.length; i++) {
-                let item = items[i];
-                item.customerId = params.customerId;
-                if (!item.deviceId || item.deviceId === '') {
-                    item.deviceId = deviceId;
-                }                
-                if (item.langId !== 'EN') {
-                    ret = await db.SaveDeviceML(item);
-                    rets.push(ret);
-                }                
-            }
-        }
-        return rets;
+        let ret = db.SetAccessDevice(params);
+        return ret;
     }
     static parse(db, data, callback) {
-        let results = [];
-        let result;
-        let dbResult;
+        let result = validate(db, data);
 
-        for (let i = 0; i < data.length; i++) {
-            dbResult = validate(db, data[i]);
-
-            result = {
-                data : dbResult.data,
-                //src: dbResult.data,
-                errors: dbResult.errors,
-                //multiple: dbResult.multiple,
-                //datasets: dbResult.datasets,
-                out: dbResult.out
-            }
-            results.push(result);
-        }
-
-        callback(results);
+        callback(result);
     }
     static entry(req, res) {
         let db = new sqldb();
@@ -208,80 +118,10 @@ api.Save = class {
 
 //#endregion
 
-//#region Implement - Delete
-
-api.Delete = class {
-    static prepare(req, res) {
-        let params = WebServer.parseReq(req).data;
-        /*
-        let customerId = secure.getCustomerId(req, res);
-        if (customerId) params.customerId = customerId;
-        params.langId = null; // force null.
-        params.branchId = null;
-        params.enabled = true;
-        */
-        return params;
-    }
-    static async call(db, params) { 
-        //return db.GetBranchs(params);
-        return null;
-    }
-    static parse(db, data, callback) {
-        let dbResult = validate(db, data);
-        let result = {}        
-        result.data = null
-        //result.src = dbResult.data
-        result.errors = dbResult.errors
-        //result.multiple = dbResult.multiple
-        //result.datasets = dbResult.datasets
-        result.out = dbResult.out
-
-        let records = dbResult.data;
-        let ret = {};
-        /*
-        records.forEach(rec => {
-            if (!ret[rec.langId]) {
-                ret[rec.langId] = []
-            }
-            let map = ret[rec.langId].map(c => c.branchId);
-            let idx = map.indexOf(rec.branchId);
-            let nobj;
-            if (idx === -1) {
-                // set id
-                nobj = {}
-                nobj.branchId = rec.branchId
-                // init lang properties list.
-                ret[rec.langId].push(nobj)
-            }
-            else {
-                nobj = ret[rec.langId][idx];
-            }
-            nobj.branchName = rec.BranchName;
-        })
-        */
-        // set to result.
-        result.data = ret;
-        callback(result);
-    }
-    static entry(req, res) {
-        let db = new sqldb();
-        let params = api.Delete.prepare(req, res);
-        let fn = async () => { return api.Delete.call(db, params); }
-        exec(db, fn).then(data => {
-            api.Delete.parse(db, data, (result) => {
-                WebServer.sendJson(req, res, result);
-            });
-        })
-    }
-}
-
-//#endregion
-
 router.use(secure.checkAccess);
 // routes for device
 router.all('/rating/device/search', api.Get.entry);
 router.post('/rating/device/save', api.Save.entry);
-//router.post('/device/delete', api.Delete.entry);
 
 const init_routes = (svr) => {
     svr.route('/customer/api/', router);
